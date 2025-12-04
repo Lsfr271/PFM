cb9872eecb5dfb1880b69d4a7d8a0377695a753b
Lsfr__ <sanno.sonson@gmail.com>
2025-12-04 21:10:48 +0400

    Update fpm_operators.hpp

--- include/fpm_operators.hpp
+++ include/fpm_operators.hpp
@@ -1,9 +1,46 @@
 #include "FPM.hpp"
 
 bool operator==(const FPM& lhs, const FPM& rhs) {
-    return lhs._fp == rhs._fp;
+    return lhs.getPermission() == rhs.getPermission();
 }
 
 bool operator!=(const FPM& lhs, const FPM& rhs){
     return !(lhs == rhs);
+}
+
+bool operator<(const FPM& lhs, const FPM& rhs)
+{
+    return lhs.getPermission() < rhs.getPermission();
+}
+
+bool operator>(const FPM& lhs, const FPM& rhs)
+{
+    return rhs < lhs;
+}
+
+bool operator<=(const FPM& lhs, const FPM& rhs)
+{
+    return !(lhs < rhs);
+}
+
+bool operator>=(const FPM& lhs, const FPM& rhs)
+{
+    return !(lhs < rhs);
+}
+
+std::ostream& operator<<(std::ostream& os, const FPM& f)
+{
+    os << f.getPermissionLevel_file();
+
+    return os;
+}
+
+FPM &FPM::operator=(const FPM &other) {
+    if (this != &other)
+    {
+        this->_fp = other._fp;
+        this->_history = other._history;
+    }
+
+    return *this;
 }
